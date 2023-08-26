5b9d375b079da517a420b7dd63f8868104d5c192
Vivek <vk88732471@gmail.com>
2023-08-26 19:16:31 +0530

    just try

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import LoadingBar from 'react-top-loading-bar'
 
 const App = () => {
   const [progress, setProgress] = useState(0);
-  
+
   return (
     <div>
       <BrowserRouter>
@@ -21,7 +21,6 @@ const App = () => {
         <Navbar />
 
         <Routes>
-
           <Route exact path="/" element={<News setProgress={setProgress} key="general" pageSize={6} country="in" con="India" category="general" />} />
           <Route exact path="/business" element={<News setProgress={setProgress} key="business" pageSize={6} country="in" con="India" category="business" />} />
           <Route exact path="/entertainment" element={<News setProgress={setProgress} key="entertainment" pageSize={6} country="in" con="India" category="entertainment" />} />
@@ -29,7 +28,6 @@ const App = () => {
           <Route exact path="/science" element={<News setProgress={setProgress} key="science" pageSize={6} country="in" con="India" category="science" />} />
           <Route exact path="/sports" element={<News setProgress={setProgress} key="sports" pageSize={6} country="in" con="India" category="sports" />} />
           <Route exact path="/technology" element={<News setProgress={setProgress} key="technology" pageSize={6} country="in" con="India" category="technology" />} />
-
           <Route exact path="/india" element={<News setProgress={setProgress} con="India" key="in" pageSize={6} country="in" category="general" />} />
           <Route exact path="/usa" element={<News setProgress={setProgress} con="Usa" key="usa" pageSize={6} country="us" category="general" />} />
           <Route exact path="/japan" element={<News setProgress={setProgress} con="Japan" key="jp" pageSize={6} country="jp" category="general" />} />
